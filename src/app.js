8e9319357d49204ecdd2811cb00d0863b658569b
Federico <nunez.fedema@gmmail.com>
2023-07-12 20:10:40 -0300

    preparando la preentrega

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { productsRouter } from './routes/products.routes.js';
+import { cartsRouter } from './routes/carts.routes.js';
+
+const port = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log(`Express server listening on port ${port}`));
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
---
+++ src/routes/products.routes.js
@@ -0,0 +1,89 @@
+import { Router } from 'express';
+import { ProductManager } from '../controllers/ProductManager.js';
+import { __dirname } from '../util.js';
+
+const router = Router();
+
+const pm = new ProductManager('../../DB/products.json');
+
+const fieldsCheck = (req, res, next) => {
+    let prod = req.body;
+    if (!prod.title || !prod.description || !prod.code || !prod.price || !prod.stock || !prod.category) return res.json({ status: 'error', message: 'Debe ingresar todos los parametros' })
+    else next();
+
+};
+
+router.post('/', fieldsCheck, async (req, res) => {
+    try {
+        let prod = req.body;
+        let newProd = {
+            title: typeof prod.title === 'string' ? prod.title : toString(prod.title),
+            description: typeof prod.description === 'string' ? prod.description : toString(prod.description),
+            code: typeof prod.code === 'string' ? prod.code : toString(prod.code),
+            price: typeof prod.price === 'number' ? prod.price : Number(prod.price),
+            status: typeof prod.status === 'boolean' ? prod.status : true,
+            stock: typeof prod.stock === 'number' ? prod.stock : Number(prod.stock),
+            category: typeof prod.category === 'string' ? prod.category : toString(prod.category),
+            thumbnail: [ prod.thumbnail ] || [ 'Sin imagen' ]
+        };
+        let productCreated = await pm.addProduct(newProd);
+        res.json({ status: 'success', data: productCreated });
+    } catch (error) {
+        res.json({ status: 'error', message: error.message });
+        throw new Error(error.message);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        let limit = Number(req.query.limit);
+        if (!limit) {
+            let result = await pm.getProducts();
+            res.json({ status: 'success', data: result })
+        } else {
+            let result = await pm.getProducts();
+            res.json({ status: 'success', data: result.slice(0, limit) });
+        }
+
+    } catch (error) {
+        res.json({ status: 'error', message: error.message });
+        throw new Error(error.message);
+    }
+});
+
+router.get('/:pid', async (req, res) => {
+    try {
+        let pid = req.params.pid;
+        let result = await pm.getProductById(pid);
+        res.json({ status: 'success', data: result });
+    } catch (error) {
+        res.json({ status: 'error', message: error.message });
+        throw new Error(error.message);
+    }
+});
+
+router.put('/:pid', fieldsCheck, async (req, res) => {
+    try {
+        let pid = req.params.pid;
+        let product = req.body;
+        let result = await pm.updateProduct(pid, product);
+        await pm.saveProducts();
+        res.json({ status: 'success', data: result });
+    } catch (error) {
+        res.json({ status: 'error', message: error.message });
+        throw new Error(error.message);
+    }
+});
+
+router.delete('/:pid', async (req, res) => {
+    try {
+        let pid = req.params.pid;
+        let result = await pm.deleteProduct(pid);
+        res.json({ status: 'success', data: result });
+    } catch (error) {
+        res.json({ status: 'error', message: error.message });
+        throw new Error(error.message);
+    }
+});
+
+export { router as productsRouter };
